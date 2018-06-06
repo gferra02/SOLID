1a81caad06f88705b04ffea38344005df778d042
gferra02 <gferra02@mail.bbk.ac.uk>
2018-06-06 15:40:36 +0100

    Added ignore for IntelliJ, Link to credit author and first code file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SOLID
-Examples of SOLID principles in Kotlin
+Examples of SOLID principles in Kotlin.
+
+Code from here: https://github.com/AdamLuisSean/OOD-Principles-Kotlin
---
+++ src/Door.kt
@@ -0,0 +1,56 @@
+interface CanBeOpened {
+    fun open()
+}
+
+interface CanBeClosed {
+    fun closed()
+}
+
+// I'm the door. I have an encapsulated state and you can change it using methods.
+final class Door : CanBeOpened, CanBeClosed {
+
+    private enum class State {
+        Open, Closed
+    }
+
+    private var state: State = State.Closed
+
+    override fun open() {
+        state = State.Open
+    }
+
+    override fun closed() {
+        state = State.Closed
+    }
+}
+
+// I'm only responsible for opening, no idea what's inside or how to close.
+class DoorOpener(door: CanBeOpened) {
+
+    val door: CanBeOpened = door
+
+    fun execute() {
+        door.open()
+        println("Door is now open")
+    }
+}
+
+// I'm only responsible for closing, no idea what's inside or how to open.
+class DoorCloser(door: CanBeClosed) {
+    val door: CanBeClosed = door
+
+    fun execute() {
+        door.closed()
+        println("Door is now closed")
+    }
+}
+
+fun main (args: Array<String>) {
+    val door = Door()
+
+    val doorOpener = DoorOpener(door)
+    doorOpener.execute()
+
+    val doorCloser = DoorCloser(door)
+    doorCloser.execute()
+}
